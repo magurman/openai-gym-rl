05b5183db743b20e2594d6b5998eb54dcaef5bd9
magurman <mattgurman@gmail.com>
2021-12-12 23:47:55 -0500

    implemented numwins avgreward for gamma

--- visualize.py
+++ visualize.py
@@ -1,24 +1,119 @@
-import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas
 
+import os 
 
-cartPoleQ = pandas.read_csv("CartPole-v0-Q-Learning.csv")
-cartPoleSarsa = pandas.read_csv("CartPole-v0-SARSA.csv")
+pathToProject = "/Users/magurman/NEU/Foundations of AI/openai-gym-rl"
 
-mountainCarQ = pandas.read_csv("MountainCar-v0-Q-Learning.csv")
-mountainCarSarsa = pandas.read_csv("MountainCar-v0-SARSA.csv")
+def plotAndSave(data, xlabel, ylabel, title, location, xticks=None, yticks=None):
+    
+    for d in data:
+        plt.plot(d[0], label=d[1])
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+
+    if xticks:
+        plt.xticks(xticks[0], xticks[1])
+
+    if yticks:
+        plt.yticks(yticks[0], yticks[1])
+
+    plt.legend()
+    plt.savefig(location)
+    plt.show()
+
+def plotGammaAvgReward(env):
+    gammas = []
+    avgRewardQ = []
+    avgRewardSarsa = []
+
+    for filename in os.listdir(pathToProject + "/diffGamma"):
+        gamma = filename[filename.index(".")-1:filename.index(".")+3]
+        df = pandas.read_csv("diffGamma/" + filename)
+        avgReward = df['reward'].mean()
+
+        if env in filename and "Q-Learning" in filename:
+            # gamma = filename[filename.index(".")-1:filename.index(".")+3]
+            df = pandas.read_csv("diffGamma/" + filename)
+            avgRewardQ.append(avgReward)
+        elif env in filename and "SARSA" in filename:
+            df = pandas.read_csv("diffGamma/" + filename)
+            avgRewardSarsa.append(avgReward)
+
+        if gamma not in gammas:
+            gammas.append(gamma)
+
+    zipped = reorganizeLabels([avgRewardQ, avgRewardSarsa], gammas)
+
+    gammas = list(list(zipped)[0])
+    avgRewardQ = list(list(zipped)[1])
+    avgRewardSarsa = list(list(zipped)[2])
+
+    data = [(avgRewardQ, "q-learning"), (avgRewardSarsa, "sarsa")]
+    xlabel = "gamma"
+    ylabel = "avg reward"
+    xticks = ([0,1,2,3], gammas)
+    title = env + " - Average Episode Reward - Q-Learning vs. Sarsa for Differing Gamma Values"
+
+    plotAndSave(data, xlabel, ylabel, title, "diffGammaPlots/" + env + "-numWins.png", xticks)
+
+def plotGammaNumWins(env):
+
+    gammas = []
+    numWinsQ = []
+    numWinsSarsa = []
+
+    for filename in os.listdir(pathToProject + "/diffGamma"):
+        gamma = filename[filename.index(".")-1:filename.index(".")+3]
+        df = pandas.read_csv("diffGamma/" + filename)
+        numWins = df['win'].values.sum()
+
+        if env in filename and "Q-Learning" in filename:
+            # gamma = filename[filename.index(".")-1:filename.index(".")+3]
+            df = pandas.read_csv("diffGamma/" + filename)
+            numWinsQ.append(numWins)
+        elif env in filename and "SARSA" in filename:
+            df = pandas.read_csv("diffGamma/" + filename)
+            numWinsSarsa.append(numWins)
+
+        if gamma not in gammas:
+            gammas.append(gamma)
+
+    zipped = reorganizeLabels([numWinsQ, numWinsSarsa], gammas)
+
+    gammas = list(list(zipped)[0])
+    numWinsSarsa = list(list(zipped)[1])
+    numWinsQ = list(list(zipped)[2])
+
+    data = [(numWinsQ, "q-learning"), (numWinsSarsa, "sarsa")]
+    xlabel = "gamma"
+    ylabel = "number of wins"
+    xticks = ([0,1,2,3], gammas)
+    title = env + "- Number of Wins - Q-Learning vs. Sarsa for Differing Gamma Values"
+
+    plotAndSave(data, xlabel, ylabel, title, "diffGammaPlots/" + env + "-numWins.png", xticks)
 
 
+def reorganizeLabels(data, labels):
+    lists = [labels]
+    for i in data:
+        lists.append(i)
 
+    listsZipped = zip(*lists)
+    sortedList = sorted(listsZipped, key= lambda pair: pair[0])
+    zipped = list(zip(*sortedList))
 
+    return zipped
 
-numWins0 = cartpole0['win'].value_counts()
-averageReward0 = cartpole0['reward'].mean()
+def main():
+    cartpole = "CartPole"
+    mountaincar = "MountainCar"
+     
+    plotGammaNumWins(cartpole)
+    plotGammaNumWins(mountaincar)
+    plotGammaAvgReward(cartpole)
+    plotGammaAvgReward(mountaincar)
 
-numWins1 = cartpole1['win'].value_counts()
-averageReward1 = cartpole1['reward'].mean()
-print(numWins0)
-print(averageReward0)
-
-print(numWins1)
-print(averageReward1)
+main()
