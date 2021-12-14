7652817b1c46ff682ee1691273db734568cf8030
Heet Sakaria <heet9022@users.noreply.github.com>
2021-12-14 17:21:47 -0500

    Actor Critic agent and model added

---
+++ agents/ActorCriticAgent.py
@@ -0,0 +1,97 @@
+import gym
+env = gym.make('LunarLander-v2')
+env = env.unwrapped
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+class ActorCritic(nn.Module):
+    def __init__(self):
+        super(ActorCritic, self).__init__()
+        self.affine = nn.Linear(8, 128)
+        
+        self.action_layer = nn.Linear(128, 4)
+        self.value_layer = nn.Linear(128, 1)
+        
+        self.logprobs = []
+        self.state_values = []
+        self.rewards = []
+
+    def forward(self, state):
+        state = torch.from_numpy(state).float()
+        state = F.relu(self.affine(state))
+        
+        state_value = self.value_layer(state)
+        
+        action_probs = F.softmax(self.action_layer(state))
+        action_distribution = Categorical(action_probs)
+        action = action_distribution.sample()
+        
+        self.logprobs.append(action_distribution.log_prob(action))
+        self.state_values.append(state_value)
+        
+        return action.item()
+    
+    def calculateLoss(self, gamma=0.99):
+        
+        # calculating discounted rewards:
+        rewards = []
+        dis_reward = 0
+        for reward in self.rewards[::-1]:
+            dis_reward = reward + gamma * dis_reward
+            rewards.insert(0, dis_reward)
+                
+        # normalizing the rewards:
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std())
+        
+        loss = 0
+        for logprob, value, reward in zip(self.logprobs, self.state_values, rewards):
+            advantage = reward  - value.item()
+            action_loss = -logprob * advantage
+            value_loss = F.smooth_l1_loss(value, reward)
+            loss += (action_loss + value_loss)   
+        return loss
+    
+    def clearMemory(self):
+        del self.logprobs[:]
+        del self.state_values[:]
+        del self.rewards[:]
+
+
+import torch
+import gym
+from PIL import Image
+
+def test(n_episodes=500, name='LunarLander_FOUR.pth'):
+    env = gym.make('LunarLander-v2')
+    policy = ActorCritic()
+    
+    policy.load_state_dict(torch.load('./preTrained/{}'.format(name)))
+    
+    render = True
+    save_gif = False
+
+#  Plot for n episodes. Print avg reward
+
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        running_reward = 0
+        for t in range(10000):
+            action = policy(state)
+            state, reward, done, _ = env.step(action)
+            running_reward += reward
+            if render:
+                 env.render()
+                 if save_gif:
+                     img = env.render(mode = 'rgb_array')
+                     img = Image.fromarray(img)
+                     img.save('./gif/{}.jpg'.format(t))
+            if done:
+                break
+        print('Episode {}\tReward: {}'.format(i_episode, running_reward))
+    env.close()
+            
+test()
